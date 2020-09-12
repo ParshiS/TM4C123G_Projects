eb57f95589f7a8c7f025f0252e25bde4fde28e41
Srinidhi Parshi <parshisrinidhi@gmail.com>
2020-09-12 01:33:42 -0400

    Update LED_GPIO.h

--- LED_GPIO.h
+++ LED_GPIO.h
@@ -15,13 +15,13 @@
 #define Pin3G	0x00000008
 
 // Define register offsets
-#define DataReg	0x00000000 // Data Register Register Offset. Note: bits 9:2 to be set when giving outputs
+#define DataReg	  0x00000000 // Data Register Register Offset. Note: bits 9:2 to be set when giving outputs
 #define Direction	0x00000400 // Direction Register Offset
-#define DigEn	0x0000051C // Digital Enable Register Offset
+#define DigEn	    0x0000051C // Digital Enable Register Offset
 #define PullUpReg	0x00000510 // Pull Up Register Offset
-#define AFSEL	0x00000420 // Alternate Function Select Register Offset
-#define AFSEL	0x00000528 // Analog Mode Select
-#define LockReg	0x00000520 // Lock, Unlock Register Offset
+#define AFSEL	    0x00000420 // Alternate Function Select Register Offset
+#define AMSEL	    0x00000528 // Analog Mode Select
+#define LockReg	  0x00000520 // Lock, Unlock Register Offset
 #define Drive2mA	0x00000500 // Drive 2mA to LED Register Offset
 #define Drive4mA	0x00000504 // Drive 4mA to LED Register Offset
 #define Drive8mA	0x00000508 // Drive 8mA to LED Register Offset
